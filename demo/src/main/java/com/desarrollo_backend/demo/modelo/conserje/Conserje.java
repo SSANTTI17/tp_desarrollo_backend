130621863abd361a96b587d183118ad83fe1053a
Barto <solaribarto@gmail.com>
2025-12-16 02:26:17 -0300

    Agregue @Entity, no se reconocia a Conserje como una entidad

--- demo/src/main/java/com/desarrollo_backend/demo/modelo/conserje/Conserje.java
+++ demo/src/main/java/com/desarrollo_backend/demo/modelo/conserje/Conserje.java
@@ -1,17 +1,34 @@
 package com.desarrollo_backend.demo.modelo.conserje;
 
+import jakarta.persistence.*; // IMPORTANTE: Esto trae @Entity, @Id, etc.
+
+@Entity
+@Table(name = "conserjes")
 public class Conserje {
 
-    private String nombre;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Autoincremental
+    private Integer id;
 
+    private String nombre;
     private String contrasenia;
 
+    // --- Constructores ---
     public Conserje() {
     }
 
     public Conserje(String nombre, String contrasenia) {
         this.nombre = nombre;
         this.contrasenia = contrasenia;
+    }
+
+    // --- Getters y Setters ---
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getNombre() {
